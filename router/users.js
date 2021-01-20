9e82eaa1cb27923de52c5e316d68e7c2369ba396
jesswinw <jesswinw@gmail.com>
2021-01-20 20:09:19 +0530

    added password hash

--- router/users.js
+++ router/users.js
@@ -1,24 +1,36 @@
 const router = require('express').Router();
-var bodyParser = require('body-parser')
+const bcrypt = require('bcrypt');
+var bodyParser = require('body-parser');
 let User = require('../models/users.model');
 
-var jsonParser = bodyParser.json()
+var jsonParser = bodyParser.json();
 
 router.route('/').get((req, res) => {
-  User.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error: ' + err));
+    User.find()
+        .then((users) => res.json(users))
+        .catch((err) => res.status(400).json('Error: ' + err));
 });
 
 router.route('/add').post(jsonParser, (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
+    const email = req.body.email;
+    const password = req.body.password;
 
-  const newUser = new User({email, password});
+    bcrypt
+        .hash(password, BCRYPT_SALT_ROUNDS)
+        .then(function (hashedPassword) {
+            password = hashedPassword;
+        })
+        .catch(function (error) {
+            console.log('Error saving user: ');
+            console.log(error);
+        });
 
-  newUser.save()
-    .then(() => res.json('User added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+    const newUser = new User({ email, hashedPassword });
+
+    newUser
+        .save()
+        .then(() => res.json('User added!'))
+        .catch((err) => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
